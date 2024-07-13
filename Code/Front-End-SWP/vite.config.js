64a9c982289562120ca93d334f026d443cbccabf
nguyentrungnam <namntse170246@fpt.edu.vn>
2024-07-13 13:23:34 +0700

    deploy

--- Code/Front-End-SWP/vite.config.js
+++ Code/Front-End-SWP/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://localhost:7155',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
 });
